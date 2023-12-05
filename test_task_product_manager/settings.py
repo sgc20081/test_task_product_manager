ba57a28fb6856cb68a841c441b6e1584f59db203
sgc20081 <sergeymarich94@gmail.com>
2023-12-05 16:28:27 +0200

    Heroku install whitehouse

--- test_task_product_manager/settings.py
+++ test_task_product_manager/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -41,6 +42,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -125,6 +127,9 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 STATIC_ROOT = BASE_DIR / 'static'
 STATIC_URL = '/static/'
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, 'static'),
+# ]
 
 SECURE_BROWSER_XSS_FILTER = True
 SECURE_CONTENT_TYPE_NOSNIFF = True
--- test_task_product_manager/wsgi.py
+++ test_task_product_manager/wsgi.py
@@ -11,6 +11,12 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
+from django.core.wsgi import get_wsgi_application
+from whitenoise import WhiteNoise
+
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'test_task_product_manager.settings')
 
 application = get_wsgi_application()
+
+application = get_wsgi_application()
+application = WhiteNoise(application)
