32ed74fb73cb1fa061b039f7a48fce00b2b60dc7
sgc20081 <sergeymarich94@gmail.com>
2023-12-16 19:08:29 +0200

    Parquet test added

--- parquet_test/views.py
+++ parquet_test/views.py
@@ -6,6 +6,7 @@ import pyarrow.parquet as pq
 import pandas as pd
 
 import base64
+import io
 
 # Create your views here.
 
@@ -15,21 +16,16 @@ def parquet_get_test(request, *args, **kwargs):
 
     data = {'column1': [1, 2, 3], 'column2': ['A', 'B', 'C'], 'column3': ['q', 'w', 'e']}
     
-    # Создаем DataFrame из словаря data
-    df = pd.DataFrame(data)
+    table = pa.Table.from_pydict(data)
 
-    # Преобразуем DataFrame в Arrow Table
-    table = pa.Table.from_pandas(df)
+    # Записываем таблицу в поток.
+    stream = io.BytesIO()
+    pq.write_table(table, stream)
 
-    buffer = pa.BufferOutputStream()
-    pq.write_table(table, buffer)
-    
-    # Получить строку из буфера
-    buffer = buffer.getvalue().to_pybytes()
-    # file = pq.ParquetFile(buffer)
-    # print(file)
-    # parquet_string = base64.b64encode(file).decode('utf-8')
+    # Возвращаем строку из потока.
+    stream.getvalue()
+    string_parquet = stream.getvalue()
 
     # print(parquet_string)
 
-    return HttpResponse(buffer, content_type='text/plain')
+    return HttpResponse(string_parquet)
